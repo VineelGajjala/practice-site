f46fcc5d9c3b8e8439c2d390999de530a3899d3f
vineelgajjala <gajjala.vineel@gmail.com>
2021-06-11 17:02:15 -0500

    added new component

--- website/src/App.vue
+++ website/src/App.vue
@@ -7,19 +7,22 @@
     <Header/>
     </v-app-bar>
 
-    <v-main>
-    </v-main>
+    <v-content>
+      <Hero/>
+    </v-content>
   </v-app>
 </template>
 
 <script>
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 export default {
   name: 'App',
 
   components: {
     Header,
+    Hero
   },
 
   data: () => ({
---
+++ website/src/components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    Hero
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Hero',
+
+    data: () => ({
+    }),
+  }
+</script>
