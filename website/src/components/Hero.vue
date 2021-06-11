0249edcff16a696d7cb79b23f38099b0f3bd519e
vineelgajjala <gajjala.vineel@gmail.com>
2021-06-11 17:10:12 -0500

    added carosel

--- website/src/components/Hero.vue
+++ website/src/components/Hero.vue
@@ -1,6 +1,16 @@
 <template>
   <v-container>
-    Hero
+    <div class="heroBlock">
+      <v-carousel>
+        <v-carousel-item
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        ></v-carousel-item>
+      </v-carousel>
+    </div>
   </v-container>
 </template>
 
@@ -8,7 +18,23 @@
   export default {
     name: 'Hero',
 
-    data: () => ({
-    }),
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+      }
+    },
   }
 </script>
